d657a22b314db98dbcfeb1db28258176f782d637
micraj <mrajzer2004@gmail.com>
2025-04-19 10:04:51 +0100

    Untested press detection logic

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 #include <tusb.h>
 #include <hid.h>
 
+#define NUM_BUTTONS 9
+
 //--------------------------------------------------------------------+
 // Macropad Main Loop
 //--------------------------------------------------------------------+
 
-bool pressed = false;
+bool switch_states[NUM_BUTTONS] = {false};
 
 int main()
 {
@@ -29,24 +31,59 @@ int main()
             char message[50];
             snprintf(message, sizeof(message), "Key pressed: %d\n", key_idx);
             uart_send_string(message);
-            if (key_idx == 1)
+
+            // Check if the key is not already pressed
+            if (!switch_states[key_idx])
             {
-                if (!pressed)
+                switch_states[key_idx] = true;
+                // Send the key press to the host
+                switch (key_idx)
                 {
+                case 1:
                     send_key_down(HID_KEY_A);
+                    break;
+                case 2:
                     send_key_down(HID_KEY_B);
+                    break;
+                case 3:
                     send_key_down(HID_KEY_C);
-                    send_release_all();
-                    pressed = true;
+                    break;
+                case 4:
+                    send_key_down(HID_KEY_D);
+                    break;
+                case 5:
+                    send_key_down(HID_KEY_E);
+                    break;
+                case 6:
+                    send_key_down(HID_KEY_F);
+                    break;
+                case 7:
+                    send_key_down(HID_KEY_G);
+                    break;
+                case 8:
+                    send_key_down(HID_KEY_H);
+                    break;
+                case 9:
+                    send_key_down(HID_KEY_I);
+                    break;
+                default:
+                    break;
                 }
             }
-            else
+
+            // Release all other keys if one is pressed
+            for (int i = 0; i < NUM_BUTTONS; i++)
             {
-                if (pressed)
+                if (i + 1 != key_idx && switch_states[i])
                 {
-                    pressed = false;
+                    switch_states[i] = false;
                 }
             }
+        }
+        else
+        {
+            // Set all to false if no key is pressed
+            memset(switch_states, 0, sizeof(switch_states));
         }
     }
     return 0;
