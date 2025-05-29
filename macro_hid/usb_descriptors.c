7384489e8e1f5f823ae8b08717d5b97f9c956b05
micraj <mrajzer2004@gmail.com>
2025-05-29 19:49:47 +0100

    Added multiple interface example and second interface config

--- macro_hid/usb_descriptors.c
+++ macro_hid/usb_descriptors.c
@@ -61,7 +61,8 @@ tusb_desc_device_t const desc_device =
         .iProduct = 0x02,
         .iSerialNumber = 0x03,
 
-        .bNumConfigurations = 0x01};
+        .bNumConfigurations = 0x01,
+};
 
 // Invoked when received GET DEVICE DESCRIPTOR
 // Application return pointer to descriptor
@@ -74,20 +75,31 @@ uint8_t const *tud_descriptor_device_cb(void)
 // HID Report Descriptor
 //--------------------------------------------------------------------+
 
-uint8_t const desc_hid_report[] =
+uint8_t const desc_hid_report1[] =
     {
         TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(REPORT_ID_KEYBOARD)),
-        TUD_HID_REPORT_DESC_GENERIC_INOUT(CFG_TUD_HID_EP_BUFSIZE, HID_REPORT_ID(REPORT_ID_CUSTOM)),
+};
 
+uint8_t const desc_hid_report2[] =
+    {
+        TUD_HID_REPORT_DESC_GENERIC_INOUT(CFG_TUD_HID_EP_BUFSIZE, HID_REPORT_ID(REPORT_ID_CUSTOM)),
 };
 
 // Invoked when received GET HID REPORT DESCRIPTOR
 // Application return pointer to descriptor
 // Descriptor contents must exist long enough for transfer to complete
-uint8_t const *tud_hid_descriptor_report_cb(uint8_t instance)
+uint8_t const *tud_hid_descriptor_report_cb(uint8_t itf)
 {
-  (void)instance;
-  return desc_hid_report;
+  if (itf == 0)
+  {
+    return desc_hid_report1;
+  }
+  else if (itf == 1)
+  {
+    return desc_hid_report2;
+  }
+
+  return NULL;
 }
 
 //--------------------------------------------------------------------+
@@ -97,13 +109,15 @@ uint8_t const *tud_hid_descriptor_report_cb(uint8_t instance)
 enum
 {
   ITF_NUM_HID,
+  ITF_NUM_PROGRAMMING,
   ITF_NUM_TOTAL
 };
 
-#define CONFIG_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_HID_INOUT_DESC_LEN) // +7 for extra endpoint descrpitor
+#define CONFIG_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN + TUD_HID_INOUT_DESC_LEN) // +7 for extra endpoint descrpitor
 
-#define EPNUM_IN 0x01
-#define EPNUM_OUT 0x81
+#define EPNUM_KEYBOARD 0x30
+#define EPNUM_PROGRAMMING_IN 0x01
+#define EPNUM_PROGRAMMING_OUT 0x81
 
 uint8_t const desc_configuration[] =
     {
@@ -111,7 +125,8 @@ uint8_t const desc_configuration[] =
         TUD_CONFIG_DESCRIPTOR(1, ITF_NUM_TOTAL, 0, CONFIG_TOTAL_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
 
         // Interface number, string index, protocol, report descriptor len, EP In address, EP Out address, size & polling interval
-        TUD_HID_INOUT_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report), EPNUM_IN, EPNUM_OUT, CFG_TUD_HID_EP_BUFSIZE, 10)};
+        TUD_HID_DESCRIPTOR(ITF_NUM_HID, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report1), EPNUM_KEYBOARD, CFG_TUD_HID_EP_BUFSIZE, 5),
+        TUD_HID_INOUT_DESCRIPTOR(ITF_NUM_PROGRAMMING, 0, HID_ITF_PROTOCOL_NONE, sizeof(desc_hid_report2), EPNUM_PROGRAMMING_IN, EPNUM_PROGRAMMING_OUT, CFG_TUD_HID_EP_BUFSIZE, 10)};
 
 #if TUD_OPT_HIGH_SPEED
 // Per USB specs: high speed capable device must report device_qualifier and other_speed_configuration
